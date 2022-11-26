7ae103bcaaa8cd9fb01a0eb4359953bee98e485f
Alesia <andomyel@yandex.ru>
2022-11-26 19:32:25 +0300

    Create main code

---
+++ ResWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿//
+
+string[] StartArray = GetStartArray(5);
+Console.Write("['");
+Console.Write(String.Join("', '", StartArray));
+Console.WriteLine("']");
+
+int LengthFinishArray = GetLengthFinishArray(StartArray);
+
+
+if ( LengthFinishArray == 0){
+    Console.WriteLine("В заданном массиве нет строк с длиной 3 символа и меньше");
+}
+else{
+string[] FinishArray = GetFinishArray(StartArray);
+Console.Write("['");
+Console.Write(String.Join("', '", FinishArray));
+Console.WriteLine("']");
+}
